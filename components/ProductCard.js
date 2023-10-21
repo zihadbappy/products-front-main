90425336b975fb9fa3caec21880e295d174e57f5
ayman-awal <ayman.awal36@gmail.com>
2023-10-20 22:24:26 -0400

    change

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,10 +1,219 @@
 
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+// import { Button, Modal, Form, Input, Select, message } from 'antd';
+// import axios from 'axios';
+
+// export const ProductCard = ({ product }) => {
+//   const {name, image, description, price, category, brand, stockQuantity} = product
+
+//   const [isModalVisible, setIsModalVisible] = useState(false);
+//   const [isDetailsModalVisible, setIsDetailsModalVisible] = useState(false);
+//   const [formData, setFormData] = useState({
+//     name,
+//     image,
+//     description,
+//     price,
+//     category,
+//     brand,
+//     stockQuantity
+//   });
+
+//   const showModal = () => {
+//     setIsModalVisible(true);
+//   };
+
+//   const showDetailsModal = () => {
+//     setIsDetailsModalVisible(true);
+//   };
+
+//   const handleCancel = () => {
+//     setIsModalVisible(false);
+//   };
+
+//   const handleDetailsModalCancel = () => {
+//     setIsDetailsModalVisible(false);
+//   }
+
+//   const handleFormChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   const handleDelete = async(e) => {
+//     e.preventDefault();
+//     try {
+//       const baseURL = 'http://localhost:5000/api/products';
+//       const response = await axios.delete(`${baseURL}/delete/${product._id}`, formData);
+//       console.log(response)
+//       if (response.status === 200) {
+//         message.success('Product deleted successfully');
+//       }
+//     } catch (error) {
+//       message.error('Error deleting product');
+//       console.error('Error Deleting product:', error);
+//     }
+//   }
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const baseURL = 'http://localhost:5000/api/products';
+//       const response = await axios.patch(`${baseURL}/edit/${product._id}`, formData);
+//       console.log(response)
+//       if (response.status === 200) {
+//         message.success('Product updated successfully');
+//       }
+//       setFormData(response)
+//     } catch (error) {
+//       message.error('Error updating product');
+//       console.error('Error updating product:', error);
+//     }
+//     setIsModalVisible(false);
+//   };
+
+//   return (
+//     <div className='product-card-container'>
+//       <div className='product-card'>
+//         <img src={image} />
+//         <div className='card-container'>
+//           <div className='card-top-sec'>
+//             <Button type="primary" style={{backgroundColor:'black'}}>
+//             {category}
+//             </Button>
+
+//             <Button type="primary" onClick={showDetailsModal}>
+//               Details
+//             </Button>
+
+//           </div>
+
+//           <div className='card-content'>
+//             <h2>{name}</h2>
+//             <p>${price}</p>
+//           </div>
+
+//           <Button type="primary" onClick={showModal} style={{backgroundColor: 'green', borderColor:'green'}}>
+//             Edit
+//           </Button>
+
+//           <Button
+//             type="primary"
+//             style={{
+//               marginTop: '10px',
+//               backgroundColor: 'red',
+//               borderColor: 'red',
+//               marginLeft: '10px',
+//             }}
+//             onClick={handleDelete}
+//           >
+//             Delete
+//           </Button>
+
+//           <Modal
+//             title="Edit Product"
+//             visible={isModalVisible}
+//             onCancel={handleCancel}
+//             footer={[
+//               <Button key="cancel" onClick={handleCancel}>
+//                 Cancel
+//               </Button>,
+//               <Button key="submit" type="primary" onClick={handleSubmit}>
+//                 Save
+//               </Button>,
+//             ]}
+//           >
+//             <Form layout="vertical">
+//               <Form.Item label="Name">
+//                 <Input name="name" value={formData.name} onChange={handleFormChange} />
+//               </Form.Item>
+//               <Form.Item label="Image">
+//                 <Input
+//                   name="image"
+//                   value={formData.image}
+//                   onChange={handleFormChange}
+//                 />
+//               </Form.Item>
+//               <Form.Item label="Description">
+//                 <Input.TextArea
+//                   name="description"
+//                   value={formData.description}
+//                   onChange={handleFormChange}
+//                 />
+//               </Form.Item>
+//               <Form.Item label="Price">
+//                 <Input name="price" value={formData.price} onChange={handleFormChange} />
+//               </Form.Item>
+//               <Form.Item label="Category">
+//                 <Select
+//                     placeholder='Select category' 
+//                     name='category'
+//                     value={formData.category}
+//                     onChange={(value) => setFormData({ ...formData, category: value })}
+//                     style={{ width: '100%' }}
+//                     options={[
+//                     {
+//                       value: 'Electronics',
+//                       label: 'Electronics',
+//                     },
+//                     {
+//                       value: 'Vehicles',
+//                       label: 'Vehicles',
+//                     },
+//                     {
+//                       value: 'Sportswear',
+//                       label: 'Sportswear',
+//                     },
+//                   ]}
+//                 />
+
+//               </Form.Item>
+
+//               <Form.Item label="Brand">
+//                 <Input name="brand" value={formData.brand} onChange={handleFormChange} />
+//               </Form.Item>
+//               <Form.Item label="Stock Quantity">
+//                 <Input
+//                   name="stockQuantity"
+//                   value={formData.stockQuantity}
+//                   onChange={handleFormChange}
+//                 />
+//               </Form.Item>
+//             </Form>
+//           </Modal>
+
+//           <Modal
+//             title="Product Details"
+//             visible={isDetailsModalVisible}
+//             onCancel={handleDetailsModalCancel}
+//             footer={[
+//               <Button key="close" onClick={handleDetailsModalCancel}>
+//                 Close
+//               </Button>,
+//             ]}
+//           >
+//             <div>
+//               <h2 style={{textAlign: 'center'}}>{name}</h2>
+//               <img src={image} style={{width: '100%'}} alt="Product Image" />
+//             </div>
+//             <div>
+//               <p>{description}</p>
+//               <p>In Stock: {stockQuantity} units</p>
+//             </div>
+//           </Modal>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+
+import React, { useState } from 'react';
 import { Button, Modal, Form, Input, Select, message } from 'antd';
 import axios from 'axios';
 
-export const ProductCard = ({ product }) => {
-  const {name, image, description, price, category, brand, stockQuantity} = product
+export const ProductCard = ({ product, removeProductFromList, updateProductInList, productList }) => {
+  const { name, image, description, price, category, brand, stockQuantity } = product;
 
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isDetailsModalVisible, setIsDetailsModalVisible] = useState(false);
@@ -15,7 +224,7 @@ export const ProductCard = ({ product }) => {
     price,
     category,
     brand,
-    stockQuantity
+    stockQuantity,
   });
 
   const showModal = () => {
@@ -32,59 +241,60 @@ export const ProductCard = ({ product }) => {
 
   const handleDetailsModalCancel = () => {
     setIsDetailsModalVisible(false);
-  }
+  };
 
   const handleFormChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleDelete = async(e) => {
+  const handleDelete = async (e) => {
     e.preventDefault();
     try {
       const baseURL = 'http://localhost:5000/api/products';
-      const response = await axios.delete(`${baseURL}/delete/${product._id}`, formData);
-      console.log(response)
+      const response = await axios.delete(`${baseURL}/delete/${product._id}`);
+      console.log(response);
       if (response.status === 200) {
         message.success('Product deleted successfully');
+        removeProductFromList(product._id);
       }
     } catch (error) {
       message.error('Error deleting product');
       console.error('Error Deleting product:', error);
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const baseURL = 'http://localhost:5000/api/products';
       const response = await axios.patch(`${baseURL}/edit/${product._id}`, formData);
-      console.log(response)
+      console.log(response);
       if (response.status === 200) {
         message.success('Product updated successfully');
       }
-      setFormData(response)
+      setFormData(response.data); 
     } catch (error) {
       message.error('Error updating product');
       console.error('Error updating product:', error);
     }
     setIsModalVisible(false);
   };
+  
 
   return (
     <div className='product-card-container'>
       <div className='product-card'>
-        <img src={image} />
+        <img src={image} alt={name} />
         <div className='card-container'>
           <div className='card-top-sec'>
-            <Button type="primary" style={{backgroundColor:'black'}}>
-            {category}
+            <Button type='primary' style={{ backgroundColor: 'black' }}>
+              {category}
             </Button>
 
-            <Button type="primary" onClick={showDetailsModal}>
+            <Button type='primary' onClick={showDetailsModal}>
               Details
             </Button>
-
           </div>
 
           <div className='card-content'>
@@ -92,12 +302,12 @@ export const ProductCard = ({ product }) => {
             <p>${price}</p>
           </div>
 
-          <Button type="primary" onClick={showModal} style={{backgroundColor: 'green', borderColor:'green'}}>
+          <Button type='primary' onClick={showModal} style={{ backgroundColor: 'green', borderColor: 'green' }}>
             Edit
           </Button>
 
           <Button
-            type="primary"
+            type='primary'
             style={{
               marginTop: '10px',
               backgroundColor: 'red',
@@ -110,47 +320,39 @@ export const ProductCard = ({ product }) => {
           </Button>
 
           <Modal
-            title="Edit Product"
+            title='Edit Product'
             visible={isModalVisible}
             onCancel={handleCancel}
             footer={[
-              <Button key="cancel" onClick={handleCancel}>
+              <Button key='cancel' onClick={handleCancel}>
                 Cancel
               </Button>,
-              <Button key="submit" type="primary" onClick={handleSubmit}>
+              <Button key='submit' type='primary' onClick={handleSubmit}>
                 Save
               </Button>,
             ]}
           >
-            <Form layout="vertical">
-              <Form.Item label="Name">
-                <Input name="name" value={formData.name} onChange={handleFormChange} />
+            <Form layout='vertical'>
+              <Form.Item label='Name'>
+                <Input name='name' value={formData.name} onChange={handleFormChange} />
               </Form.Item>
-              <Form.Item label="Image">
-                <Input
-                  name="image"
-                  value={formData.image}
-                  onChange={handleFormChange}
-                />
+              <Form.Item label='Image'>
+                <Input name='image' value={formData.image} onChange={handleFormChange} />
               </Form.Item>
-              <Form.Item label="Description">
-                <Input.TextArea
-                  name="description"
-                  value={formData.description}
-                  onChange={handleFormChange}
-                />
+              <Form.Item label='Description'>
+                <Input.TextArea name='description' value={formData.description} onChange={handleFormChange} />
               </Form.Item>
-              <Form.Item label="Price">
-                <Input name="price" value={formData.price} onChange={handleFormChange} />
+              <Form.Item label='Price'>
+                <Input name='price' value={formData.price} onChange={handleFormChange} />
               </Form.Item>
-              <Form.Item label="Category">
+              <Form.Item label='Category'>
                 <Select
-                    placeholder='Select category' 
-                    name='category'
-                    value={formData.category}
-                    onChange={(value) => setFormData({ ...formData, category: value })}
-                    style={{ width: '100%' }}
-                    options={[
+                  placeholder='Select category'
+                  name='category'
+                  value={formData.category}
+                  onChange={(value) => setFormData({ ...formData, category: value })}
+                  style={{ width: '100%' }}
+                  options={[
                     {
                       value: 'Electronics',
                       label: 'Electronics',
@@ -165,15 +367,14 @@ export const ProductCard = ({ product }) => {
                     },
                   ]}
                 />
-
               </Form.Item>
 
-              <Form.Item label="Brand">
-                <Input name="brand" value={formData.brand} onChange={handleFormChange} />
+              <Form.Item label='Brand'>
+                <Input name='brand' value={formData.brand} onChange={handleFormChange} />
               </Form.Item>
-              <Form.Item label="Stock Quantity">
+              <Form.Item label='Stock Quantity'>
                 <Input
-                  name="stockQuantity"
+                  name='stockQuantity'
                   value={formData.stockQuantity}
                   onChange={handleFormChange}
                 />
@@ -182,18 +383,18 @@ export const ProductCard = ({ product }) => {
           </Modal>
 
           <Modal
-            title="Product Details"
+            title='Product Details'
             visible={isDetailsModalVisible}
             onCancel={handleDetailsModalCancel}
             footer={[
-              <Button key="close" onClick={handleDetailsModalCancel}>
+              <Button key='close' onClick={handleDetailsModalCancel}>
                 Close
               </Button>,
             ]}
           >
             <div>
-              <h2 style={{textAlign: 'center'}}>{name}</h2>
-              <img src={image} style={{width: '100%'}} alt="Product Image" />
+              <h2 style={{ textAlign: 'center' }}>{name}</h2>
+              <img src={image} style={{ width: '100%' }} alt='Product Image' />
             </div>
             <div>
               <p>{description}</p>
@@ -205,5 +406,3 @@ export const ProductCard = ({ product }) => {
     </div>
   );
 };
-
-
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,20 +1,26 @@
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ProductCard } from '../../components/ProductCard';
 import { Button } from 'antd';
 import { useRouter } from 'next/router';
 
-
 const Products = () => {
-    const router = useRouter();
-  const [productList, setProductList] = useState([]);   
+  const router = useRouter();
+  const [productList, setProductList] = useState([]);
+
+  const removeProductFromList = (productId) => {
+    setProductList((prevProductList) =>
+      prevProductList.filter((product) => product._id !== productId)
+    );
+  };
 
   useEffect(() => {
     async function fetchData() {
       try {
         const baseURL = 'http://localhost:5000/api/products';
         const response = await axios.get(baseURL);
-        console.log(response.data)
+        console.log(response.data);
         setProductList(response.data);
       } catch (error) {
         console.error('Error fetching products:', error);
@@ -26,41 +32,47 @@ const Products = () => {
 
   return (
     <>
-    <h1 style={{textAlign: 'center'}}>All Products</h1>
-    <div className='products-list-container'>
-        
+      <h1 style={{ textAlign: 'center' }}>All Products</h1>
+      <div className='products-list-container'>
         <div className='products'>
-            <div className='product-center'>
-            {productList.message === "There are no products" ? (
+          <div className='product-center'>
+            {productList.message === 'There are no products' ? (
               <div className='product-row'>
                 <p>{productList.message}</p>
               </div>
             ) : (
-                <div className='product-grid'>
-                    {
-                        productList.map((product) => <div className='product-row'>
-                            <ProductCard product={product} />
-                        </div>)
-                    }
-                </div>
-              )}
-            </div>
+              <div className='product-grid'>
+                {productList.map((product) => (
+                  <div className='product-row' key={product._id}>
+                    <ProductCard
+                      product={product}
+                      removeProductFromList={removeProductFromList}
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-        <div style={{ display: 'flex', alignItems:'center', justifyContent: 'center', margin: '30px 0' }}>
-            <Button
-                type="primary"
-                style={{ marginTop: "10px", backgroundColor: 'red', borderColor: 'red' }}
-                onClick={() => router.push('/')}
-            >
-                Go Home
-            </Button>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: '30px 0',
+          }}
+        >
+          <Button
+            type='primary'
+            style={{ marginTop: '10px', backgroundColor: 'red', borderColor: 'red' }}
+            onClick={() => router.push('/')}
+          >
+            Go Home
+          </Button>
         </div>
-        
-    </div>
+      </div>
     </>
   );
 };
 
 export default Products;
-
-
